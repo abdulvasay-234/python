f134d3a6fcf7c07fedab79a2c162971f3dd50c79
Afnan abdul vasay <afnanabdulvasay234@gmail.com>
2022-09-20 20:02:26 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,7 @@
+# STRINGS
+course = "Python for beginers"   # the string in python is technically real world object
+print(course.upper())
+print(course.lower())
+print(course.replace("h" , "H"))
+print(course.find("Python"))
+print("Python" in course)
