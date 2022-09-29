1960e85f915d893b204df25e424e6ed6e1e1e60c
Afnan abdul vasay <afnanabdulvasay234@gmail.com>
2022-09-29 22:12:18 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+#VARIBLE
+#it is the value which we give to identify a certain value
+name = "Afnan"
+age = 18
+college = "OX University"
+information = True
+print(name)
+print(age)
+print(college)
+print(information)
